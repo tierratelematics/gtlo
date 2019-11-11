3de61a372b4b2353603f63ee1ec80d3057fbd143
Andrea Ratto <aratto@topcon.com>
2019-11-11 09:30:33 +0100

    Only export interfaces at top level

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export * from "./interfaces";
 import { automagic } from "./config";
+import { LoggerFactory } from "./interfaces";
 import { DefaultLoggerFactory } from "./LoggerFactory";
 
-export const loggerFactory = new DefaultLoggerFactory();
+export * from "./interfaces";
+export const loggerFactory: LoggerFactory = new DefaultLoggerFactory();
 automagic(loggerFactory);
